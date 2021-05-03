ded58d112988527d0eafc3242e3d21274be4f326
Joel Saavedra <j.saavedra.l@gmail.com>
2021-05-03 07:55:28 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn SpryTrack
         </a>
       </header>
     </div>
