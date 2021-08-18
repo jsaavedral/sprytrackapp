5330b4d37c2067666c7a96431637ed6d87afdcb1
joelglunscorp <joel.gluns.corp@gmail.com>
2021-08-18 22:32:21 +0300

    test

--- src/App.js
+++ src/App.js
@@ -9,31 +9,6 @@ function App() {
         <p>
           Cheap SPA (Single Page Application) Websites using AWS Amazon Web Services!
         </p>
-        {/* <a
-          className="App-link"
-          href="https://aws.amazon.com/s3/pricing/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Check out: Amazon AWS S3 Princing
-        </a>
-        <a
-          className="App-link"
-          href="https://aws.amazon.com/free/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Check out: Amazon AWS Free Tier
-        </a>
-        <a 
-          className="App-link"
-          href="https://aws.amazon.com/amplify/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Check out: Amazon AWS Amplify
-        </a>
-        */}
         <a href="mailto:cheap.spa.web@gmail.com?subject=Information Request&body=Hi, please send me more information about Cheap SPA Websites. Thank you! Kind regards!" style="text-decoration:none">Send Email</a>
       </header>
     </div>
