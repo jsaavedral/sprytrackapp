a4c04c6778bb8e22a4070f826747867750484f3e
joelglunscorp <joel.gluns.corp@gmail.com>
2021-08-18 22:43:41 +0300

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Cheap SPA (Single Page Application) Websites using AWS Amazon Web Services!
         </p>
         <a
           className="App-link"
